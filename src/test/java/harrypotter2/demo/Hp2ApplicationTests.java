9184822672adc97fc73b075b26ac73cc28aeae3f
Ramiro <rrcolumbia21@gmail.com>
2023-05-14 10:32:40 +0100

    start

---
+++ src/test/java/harrypotter2/demo/Hp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package harrypotter2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
