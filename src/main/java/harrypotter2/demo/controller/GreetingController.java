7b8a42615ccb53ebf0d4746cf12beac61e6e586e
Ramiro <rrcolumbia21@gmail.com>
2023-05-15 10:21:28 +0100

    greeting controller

--- src/main/java/harrypotter2/demo/controller/GreetingController.java
+++ src/main/java/harrypotter2/demo/controller/GreetingController.java
@@ -4,5 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
 public class GreetingController {
+
+    @GetMapping
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="Witches and Wizards") String name,
+                           Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
 }
