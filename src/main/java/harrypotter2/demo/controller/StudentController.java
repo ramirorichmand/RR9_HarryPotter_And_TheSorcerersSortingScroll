ce2a0e40a0376c7ed0f8fece8ecb29362f89853c
Ramiro <rrcolumbia21@gmail.com>
2023-05-15 10:25:10 +0100

    student

--- src/main/java/harrypotter2/demo/controller/StudentController.java
+++ src/main/java/harrypotter2/demo/controller/StudentController.java
@@ -1,4 +1,15 @@
 package harrypotter2.demo.controller;
 
+import harrypotter2.demo.model.Room;
+import harrypotter2.demo.model.Student;
+import harrypotter2.demo.model.types.HouseType;
+import harrypotter2.demo.model.types.PetType;
+import harrypotter2.demo.repository.RoomRepository;
+import harrypotter2.demo.repository.StudentRepository;
+import harrypotter2.demo.service.RoomService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 public class StudentController {
 }
