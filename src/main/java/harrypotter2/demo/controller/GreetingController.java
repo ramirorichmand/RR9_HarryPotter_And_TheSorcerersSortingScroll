d8d4895ea62be82e84c688c5aa9d1005b2a3f7ef
Ramiro <rrcolumbia21@gmail.com>
2023-05-15 10:21:01 +0100

    dependencies fixed

--- src/main/java/harrypotter2/demo/controller/GreetingController.java
+++ src/main/java/harrypotter2/demo/controller/GreetingController.java
@@ -1,4 +1,8 @@
 package harrypotter2.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 public class GreetingController {
 }
