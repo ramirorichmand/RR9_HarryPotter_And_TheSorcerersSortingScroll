3c1e7c96d1c32e30cc9105da469446ecfa3d0f2d
Ramiro <rrcolumbia21@gmail.com>
2023-05-15 10:23:33 +0100

    room mapping

--- src/main/java/harrypotter2/demo/controller/RoomController.java
+++ src/main/java/harrypotter2/demo/controller/RoomController.java
@@ -9,5 +9,66 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@RestController
+@RequestMapping("/")
 public class RoomController {
+    private final RoomService roomService;
+    private final RoomRepository roomRepository;
+
+    public RoomController(RoomService roomService, RoomRepository roomRepository) {
+        this.roomService = roomService;
+        this.roomRepository = roomRepository;
+    }
+
+    @GetMapping("rooms")
+    public List<Room> get() {
+        return roomRepository.findAll();
+    }
+
+    @PostMapping("rooms")
+    public Room post() {
+        return roomRepository.save(roomService.createNewEmptyRoom());
+    }
+
+    @GetMapping("rooms/{roomId}")
+    public Optional<Room> getRoom(@PathVariable Long roomId) throws RoomNotFound {
+        if (roomRepository.existsById(roomId)) {
+            return roomRepository.findById(roomId);
+        } else {
+            throw new RoomNotFound("This room doesn't exist.");
+        }
+    }
+
+    @DeleteMapping("rooms/{roomId}")
+    public void deleteRoom(@PathVariable Long roomId) throws RoomNotFound {
+        if (roomRepository.existsById(roomId)) {
+            roomRepository.deleteById(roomId);
+        } else {
+            throw new RoomNotFound("This room doesn't exist.");
+        }
+    }
+
+    @PutMapping("rooms/{roomId}")
+    public Room updateRoom(@PathVariable Long roomId) throws RoomNotFound {
+        Room room = roomRepository.findById(roomId).orElseThrow(() -> new RoomNotFound("This room does not exist."));
+        room.setHouseType(HouseType.RAVENCLAW);
+        return roomRepository.save(room);
+    }
+
+    @PutMapping("assignStudent/{studentId}")
+    public void assignStudent(@PathVariable Long studentId) throws StudentNotFound {
+        roomService.assignStudentToRoom(studentId);
+    }
+
+    @GetMapping("rooms/available")
+    public List<Room> getEmptyRooms() {
+        //return roomService.getEmptyRooms(); -- also a possibility
+        return roomRepository.getRoomsByStudentsEmpty();
+    }
+
+    @GetMapping("/rooms/rat-owners")
+    public List<Room> roomsWithoutRatOwners() {
+        return roomService.findSafeRoomForRonsRat();
+    }
 }
+
