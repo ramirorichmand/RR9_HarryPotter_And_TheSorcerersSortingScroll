f1d729c9c2e2e05dbc3615b7d5f18573bff7450b
Ramiro <rrcolumbia21@gmail.com>
2023-05-15 10:22:57 +0100

    room contrls

--- src/main/java/harrypotter2/demo/controller/RoomController.java
+++ src/main/java/harrypotter2/demo/controller/RoomController.java
@@ -1,4 +1,13 @@
 package harrypotter2.demo.controller;
 
+import harrypotter2.demo.model.Room;
+import harrypotter2.demo.model.types.HouseType;
+import harrypotter2.demo.repository.RoomRepository;
+import harrypotter2.demo.service.RoomService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
 public class RoomController {
 }
