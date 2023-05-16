ba2eefe3301530dc5d503da9a6af19272d32e626
Ramiro <rrcolumbia21@gmail.com>
2023-05-16 08:05:46 +0100

    room model 1 to many

--- src/main/java/harrypotter2/demo/model/Room.java
+++ src/main/java/harrypotter2/demo/model/Room.java
@@ -1,4 +1,37 @@
 package harrypotter2.demo.model;
 
+import harrypotter2.demo.model.types.HouseType;
+import harrypotter2.demo.model.types.PetType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
 public class Room {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private HouseType houseType;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    private Set<Student> students = new HashSet<>();
+
+
+    public Room(HouseType houseType, Set<Student> students) {
+        this.houseType = houseType;
+        this.students = students;
+    }
+
 }
