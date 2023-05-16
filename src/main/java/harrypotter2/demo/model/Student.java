83acb5ab811346a93b546c0b324ea800ddc5ec4b
Ramiro <rrcolumbia21@gmail.com>
2023-05-16 08:07:10 +0100

    student

--- src/main/java/harrypotter2/demo/model/Student.java
+++ src/main/java/harrypotter2/demo/model/Student.java
@@ -1,4 +1,34 @@
 package harrypotter2.demo.model;
 
+import harrypotter2.demo.model.types.PetType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
 public class Student {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private PetType petType;
+
+    public Student(String name, PetType petType) {
+        this.name = name;
+        this.petType = petType;
+    }
+
 }
