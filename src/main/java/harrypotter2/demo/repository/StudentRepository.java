be7c2a3d3a72fcf44aaff9fc6bc91a040b41eb4b
Ramiro <rrcolumbia21@gmail.com>
2023-05-17 08:02:07 +0100

    student and room

--- src/main/java/harrypotter2/demo/repository/StudentRepository.java
+++ src/main/java/harrypotter2/demo/repository/StudentRepository.java
@@ -1,4 +1,9 @@
 package harrypotter2.demo.repository;
 
-public class StudentRepository {
+import harrypotter2.demo.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
 }
