0d91fb45cb6dbc57c5f2ba28984130e0b510a63e
Ramiro <rrcolumbia21@gmail.com>
2023-05-17 08:03:03 +0100

    error in model fixed

--- src/main/java/harrypotter2/demo/model/Student.java
+++ src/main/java/harrypotter2/demo/model/Student.java
@@ -9,10 +9,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
